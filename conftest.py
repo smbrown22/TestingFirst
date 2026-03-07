88cb4c279027d4e9cf192283ec153af5a4d83354
smbrown22 <smbrown22@my.waketech.edu>
2026-03-07 03:16:35 +0000

    first commit

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from health import take_damage, heal, is_alive
+
+@pytest.fixture
+def player():
+    return {"health" : 100, "max_health": 100, "alive": True}
+
+def test_take_damage(player):
+    result = take_damage(player, 30)
+    assert result["health"] == 70
+
+def test_player_dies(player):
+    result = take_damage(player, 100)
+    assert result["alive"] == False
---
+++ health.py
@@ -0,0 +1,19 @@
+def player():
+    return {"health" : 100, "max_health": 100, "alive": True}
+
+def take_damage(player, amount):
+    player["health"] = max(0, player["health"] - amount)
+    if player["health"] == 0:
+        player["alive"] = False
+    return player
+
+
+def heal(player, amount):
+    if not player["alive"]:
+        return player
+    player["health"] = min(player["max_health"], player["health"] + amount)
+    return player
+
+
+def is_alive(player):
+    return player["alive"] and player["health"] > 0
