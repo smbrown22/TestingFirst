1459b471e81ec8ee34d9fd57d085f9ffb733c8d9
smbrown22 <smbrown22@my.waketech.edu>
2026-03-26 12:13:01 +0000

    is this what you mean by two lines

--- conftest.py
+++ conftest.py
@@ -1,13 +1,16 @@
 import pytest
 from health import take_damage
 
+
 @pytest.fixture
 def player():
     return {"health" : 100, "max_health": 100, "alive": True}
 
+
 def test_take_damage(player):
     result = take_damage(player, 30)
     assert result["health"] == 70
+
 
 def test_player_dies(player):
     result = take_damage(player, 100)
